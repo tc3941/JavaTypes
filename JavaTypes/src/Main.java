2cfbcc687be90539c68ffd419d1c13c8e8694700
Teddy Conyers Jr <tc3941@rit.edu>
2023-08-01 18:47:45 -0400

    Add files via upload

---
+++ JavaTypes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        final int minAge;
+        int FINAL_AGE;
+        int decimal = 2.3;
+
+        System.out.println(minAge);
+     Fail(FINAL_AGE);
+
+    }
+    void Fail(){
+
+    }
+}
